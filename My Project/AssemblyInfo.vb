4bd073502ae48a76261363ea08dd628ddc00a4f4
Andreas Josefsson <andreas@barnensbibliotek.se>
2017-03-31 11:56:18 +0200

    first commit

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("KulturkatalogenLog")>
+<Assembly: AssemblyDescription("Andreas kulturkatalog loghandler. tar hand om diarie och loggar i kulturkatalogen")>
+<Assembly: AssemblyCompany("Kultur i Väst")>
+<Assembly: AssemblyProduct("KulturkatalogenLog")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76fd7ed7-8f48-4a2d-8655-33c7fe098e16")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ kulturkatalogLogController.vb
@@ -0,0 +1,3 @@
+﻿Public Class kulturkatalogLogController
+
+End Class
